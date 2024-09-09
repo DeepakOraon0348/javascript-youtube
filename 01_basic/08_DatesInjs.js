2cc18a1754b539c2f3131419d52f9c24cb6ceff9
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-09 08:16:14 +0000

    date in js

---
+++ 01_basic/08_DatesInjs.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);//2024-09-09T08:00:51.972Z
+// console.log(myDate.toString());//Mon Sep 09 2024 08:00:51 GMT+0000
+// console.log(myDate.toDateString());//Mon Sep 09 2024
+// console.log(myDate.toLocaleString());//9/9/2024, 8:00:51 AM
+// console.log(typeof myDate);//ye interview me puchhta hii->object(o/p)
+
+// let myCurrentDate = new Date();// ye current date nahi deta hii.
+// console.log(myCurrentDate.toDateString());
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate1 = new Date(2023,0,23,5,3)//ye time vi deta hii.
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate1 = new Date("2023,01,23")
+console.log();
+
+
+
