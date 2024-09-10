7b28b4c76d523d6de176f117a1443be4dede70ae
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-10 08:42:28 +0000

    Convert into array

--- 02_basic/02_array1.js
+++ 02_basic/02_array1.js
@@ -9,6 +9,31 @@ const dc_heros =["superman","flash","batman"]
 
 const newarry=marvel_heros.concat(dc_heros);
 //this right way to marge the array.
-console.log(newarry);
+//console.log(newarry);
+
+//++++++ spread method +++++
+const all_new_heros=[...marvel_heros,...dc_heros]
+// each array value ko sprid kar deta hii.
+//console.log(all_new_heros);
+
+const another_array=[1,2,3,[4,5,6,],7,[6,7[4,5]]]
+const real_another_array=another_array.flat(Infinity)
+//flat method sab defth array ko concat kar deta hii.
+console.log(real_another_array);
+
+console.log(Array.isArray("Deepak"));//array hii ya nahi pata karne ke liye.
+console.log(Array.from("Deepak"));//kisi vi string ya object ko array me convert karne ke liye.
+console.log(Array.from({name:"deepak"}));//ye interesting hii interview prosfective se .
+//ye impty array deta hii directly convert nahi kar pata hii.
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,scor));
+
+
+
+
 
 
