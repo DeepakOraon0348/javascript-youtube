1ffe6cc2cbdc85360e27e4f1c56678bba21b3e40
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-10 06:40:43 +0000

    Array Declaration

--- 02_basic/01_Array.js
+++ 02_basic/01_Array.js
@@ -1,3 +1,13 @@
 //Array
 //[]->squar breaket,()->prenthesis,[]->breaket,{}->breses/curly bress
-const myArr = [0,1,2,3,4,5];
+const myArr = [0,1,2,3,4,5,"Deepak"];//ye ek object type hii.
+
+console.log(myArr[6]);//array ko hum esh parkar aceses kar sakte hii.
+//array always start from 0 index. or esh me diffrent data type ka value vi rakh sakte hii.
+const myHrors =["shaktiman","naagraj"]
+
+const myArr2= new Array(1,2,3,4)//array ko hum yeshe vi declear kar sakte hii.
+console.log(myArr2[1]);
+
+
+
