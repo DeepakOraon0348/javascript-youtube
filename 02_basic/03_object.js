6aa7dfd564bd889525c3da97916b1e677713c6b7
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-10 11:38:27 +0000

    use function in object

--- 02_basic/03_object.js
+++ 02_basic/03_object.js
@@ -3,21 +3,61 @@
 
 
 //object literals
+//symbol declaration
+
+const mySym=Symbol("key1")
+
+
 //object creation
 const JsUser={
     name:"Deepak",
     "full name":"Deepak Oraon",
+    [mySym]: "mykey1",//symbol yese declear hota hii object me.
     age:18,
     location:"Seraikella",
     email:"hitesh@google.com",
     isLoggedIn:false,
     lastLoginDays:["Monday","Saturday"]
 }//object ko allwas uske key ke though access kiya ja sakta hii.
+//object me keys and value hota hii.
 
 //object access technique
-console.log(JsUser.email);
-console.log(JsUser["email"]);//ye jab key string me diya jata hii tab 
+//console.log(JsUser.email);
+//console.log(JsUser["email"]),//ye jab key string me diya jata hii tab 
 //usko esh parkar access kiya jata hii.
-console.log(JsUser["full name"]);
+// console.log(JsUser["full name"]);
+// console.log( typeof mySym);//ye symbol hii
+// console.log(typeof JsUser[mySym]);//yesh symbol ke andar string value hii.
+//ye interview question hii.
+//console.log(JsUser);
+
+
+JsUser.email="hitesh@chatgpt.com"
+//Object.freeze(JsUser)//object ko freeze(lock) karne ke liye.object.freeze ka paryog karte hii.
+JsUser.email="hitesh@chatgptDeepak.com"
+JsUser.name="hitesh kumar"
+JsUser["full name"]="Rahul kumar"
+//console.log(JsUser);
+
+JsUser.greeting=function(){
+    console.log("Hello js user");
+}
+
+JsUser.greetingTwo=function(){
+    console.log(`Hello js user $(this.name)`);
+}
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo);
+
+
+
+
+
+
+
+
+
+
+
 
 
