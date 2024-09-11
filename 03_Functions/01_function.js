460d053570d808f34ad416c3fa62da5932d282b9
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-11 08:49:44 +0000

    object in functon

--- 03_Functions/01_function.js
+++ 03_Functions/01_function.js
@@ -46,9 +46,44 @@ function loginUserMessage(username="sam"){//ye empty value me under nahijayega.
 
 // loginUserMessage("hitesh")//no result becouse return value ko kahi store nahi kar rahe hii.
 // console.log(loginUserMessage("hitesh"));
-console.log(loginUserMessage());//o/p->undefind,undefined just logged in
+//console.log(loginUserMessage());//o/p->undefind,undefined just logged in
 
 //+++++++ use of if statement++
+//function me multiple value kaise pass hoti hii.
+// 
+
+//+++ ye interview me puchta hii.
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+} //o/p->[ 400, 500 ]
+
+//console.log(calculateCartPrice(200,300,400,500));//then o/p->[ 200, 300, 400 ]
+//+++++++++++ object ko function me kaise likhte hii.
+const user={
+    username:"hitesh",
+    prices:199//so type script is very important
+}
+
+function handleObject(anyobject){
+    console.log(`usrname is ${anyobject.username} and price is ${anyobject.price}`);
+
+    
+}
+//handleObject(user);
+//other way to call object
+handleObject({
+    username:"Deepak",
+    price:399
+})
+//++++++++ Array ko kaise call karte hii in function me.
+const myNewArray=[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}//o/p->400
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));//both are same.
 
 
 
