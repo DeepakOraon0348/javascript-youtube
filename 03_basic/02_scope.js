31a13c06eb1ff1fc3197e5f077eb214c937d439a
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-11 09:34:24 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,13 @@
+
+//problem yaha pe aata hii.
+if(true){
+    let a=10
+    const b=20
+    var c=30
+}
+
+//console.log(a);// a is not defined
+//console.log(b);// b is not defined
+console.log(c);
+
+
