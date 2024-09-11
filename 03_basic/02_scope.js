4c5a4bad55f62001bb8c6625f290de9c0e2d4792
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-11 12:05:17 +0000

    hosting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -16,8 +16,47 @@ let a=300//ye hii global scope
 if(true){
     let a=10
     const b=20
-    console.log("INNER: ",a);//ye  hii block/local scope
+    //console.log("INNER: ",a);//ye  hii block/local scope
 }
-console.log(a);
+//console.log(a);
 
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website="youtube"
+        //console.log(username);
+        
+    }
+    //console.log(website);//ye undefined error through karega.
+    two()
+    
+} 
+//one()
+
+if(true){
+    const username="Deepak"
+    if(username==="Deepak"){
+        const website=" youtube"
+        //console.log(username+website);
+        
+    }
+    //console.log(website);//ye error through karega not defind ka
+    
+}
+//console.log(username);//ye vi error through karega not defind
+
+//++++++++++++++ Interesting ++++++++++++++
+console.log(addone(5))//ye exicute hota hii.
+
+function addone(num){
+    return num+1
+}
+
+addTwo(5)//ye error through karta hii.Cannot access 'addTwo' before initialization
+
+const addTwo=function(num){//yaha pe addTwo ko expresion vi bola jata hii.
+    return num+2
+}
+//eshe hosting bolte hii.
 
