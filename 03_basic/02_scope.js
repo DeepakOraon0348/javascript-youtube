c07737cef52bac59217413fbe59b6334a1a0d35e
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-11 11:32:24 +0000

    global & block scope

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -4,10 +4,20 @@ if(true){
     let a=10
     const b=20
     var c=30
+    //c=30//dono same hii.
 }
 
 //console.log(a);// a is not defined
 //console.log(b);// b is not defined
-console.log(c);
+//console.log(c);//ye access vi karta hii or output vi deta hii.
+//yeshiliye var ka paryog nahi karte hii.
+
+let a=300//ye hii global scope 
+if(true){
+    let a=10
+    const b=20
+    console.log("INNER: ",a);//ye  hii block/local scope
+}
+console.log(a);
 
 
