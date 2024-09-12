2bdc0fd1854f8392333da3094ac12a308b49745f
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-12 14:19:22 +0000

    if statement

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,19 @@
+//if->statement
+
+// if(true){
+//   // yeha pe ye exicute hoga
+// }
+// if(false){
+//     //yeha pe ye exicute nahi hoga.
+// }
+
+const isUserloggedIn=true
+
+// if(2=="2"){
+//     console.log("executed");//o/p->executed
+// }
+
+if(2==="2"){
+    console.log("executed");//no execution
+}
+/** here some conditional opertor(<,>,<=,>=,!=,===) */
