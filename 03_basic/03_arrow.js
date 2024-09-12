68cb3bf8b0f6194760dcfc5009d8faea76240d04
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-12 09:14:48 +0000

    arrow function

--- 03_basic/03_arrow.js
+++ 03_basic/03_arrow.js
@@ -4,14 +4,53 @@ const user={
 
     welcomeMessage:function(){
         console.log(`${this.username}, welcome to website`);//this key word current contast ko refer hota hii.
-        console.log(this);
+        console.log(this);//all contask ko out put karta hii.
         
     }
 }
 
 //user.welcomeMessage()
-//user.username="sam"//ye current contast hii ye change ho jaye ga.
+//user.username="sam"//ye current contask hii ye change ho jaye ga.
 //o/p->sam, welcome to website
 // user.welcomeMessage()
 
-console.log(this);//ye empty object deta hii o/p->{}
+//console.log(this);//ye empty object deta hii o/p->{} , but browser ke andar window object print karta hii
+
+// function chai(){
+//     let username="hitesh"
+//     //console.log(this);//yeha pe ye global object deta hii.
+//     console.log(this.username);//yeha pe ye access nahi deta hii.
+//     //undefind output deta hii.
+    
+// }
+// chai()
+
+//++++++++++++ Arrow Function +++++++++++++
+// const chai=function(){
+//     let username="hetesh"
+//     console.log(this.username);
+// }// ye undefind output de raha hii.
+
+
+const chai=()=>{//ye ban gya arrow function.
+    let username="hetesh"
+    console.log(this.username);
+}
+
+
+//chai()
+
+//syntax of arrow function
+//()=>{}
+
+// const addTwo=(num1,num2)=>{//ye basic arrow function hii.
+//     return num1+num2//yeshe explisit return type vi bolte hii.
+// } 
+
+
+//const addTwo=(num1,num2)=> num1+num2//ye implisit return function hii.same result deta hii.
+//const addTwo=(num1,num2)=> (num1+num2)
+
+const addTwo=(num1,num2)=>({username:"Deepak"})//object ko ap karly brecs se app return nahi karsakte hii.
+
+console.log(addTwo(3,4));
