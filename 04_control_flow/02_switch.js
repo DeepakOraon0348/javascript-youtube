e4c8d27dbc104109df6447249ca03cd60b98c871
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-09-13 12:11:06 +0000

    switch loop only

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 2:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+// const month = "march"
+// switch (month) {
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("aprial");
+//         break;
+
+//     default:
+//         console.log("default value match");
+//         break;
+// }
